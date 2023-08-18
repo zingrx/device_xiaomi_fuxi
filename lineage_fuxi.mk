2f74747ab32e56995db3b36c39a904a9618186e8
Zinger <mnitpro@gmail.com>
2023-08-18 07:37:30 +0300

    Update build flags

--- lineage_fuxi.mk
+++ lineage_fuxi.mk
@@ -30,49 +30,29 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit device configurations
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
 
-## Device identifier
+# Build Flags
+TARGET_HAS_UDFPS := true
+TARGET_ENABLE_BLUR := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_OPTOUT_GOOGLE_TELEPHONY := false
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# GAPPS
+WITH_GMS := true
+
+# ROM Flags
+TARGET_BUILD_GRAPHENEOS_CAMERA := true
+RISING_CHIPSET := Snapdragon-888-Gen2
+RISING_MAINTAINER := zinger
+
+# Device Flags
 PRODUCT_DEVICE := fuxi
 PRODUCT_NAME := lineage_fuxi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := 2211133C
 PRODUCT_MANUFACTURER := Xiaomi
-
-# GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# riceDroid Flags
-RICE_CHIPSET := Snapdragon-888-Gen2
-RICE_OFFICIAL := false
-
-# Sushi Bootanimation (only 720/1080/1440 supported. if not defined, bootanimation is google bootanimation)
-SUSHI_BOOTANIMATION := 1080
-
-# Graphene Camera
-#TARGET_BUILD_GRAPHENEOS_CAMERA := true
-
-# disable/enable blur support, default is false
-TARGET_ENABLE_BLUR := true
-
-# UDFPS ICONS/ANIMATIONS
-TARGET_HAS_UDFPS := true
-
-# Spoof build description/fingerprint as pixel device
-TARGET_USE_PIXEL_FINGERPRINT := false
-
-# GMS build flags, if none were defined the package build type will be AOSP (default: false)
-WITH_GMS := true
-
-# Customized GMS Flags 
-TARGET_GAPPS_ARCH := arm64
-
-# Opt out of google dialer support, compiler will build aosp dialer,
-TARGET_OPTOUT_GOOGLE_TELEPHONY := false
-
-# Quick Tap 
-TARGET_SUPPORTS_QUICK_TAP := true
-
-#Face unlock 
-TARGET_FACE_UNLOCK_SUPPORTED := true
-
-# Maintainer Stuff
-RICE_MAINTAINER := zinger
+# Build Fingerprint
+TARGET_USE_PIXEL_FINGERPRINT := false 
