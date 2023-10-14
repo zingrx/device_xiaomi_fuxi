1611d71f6bd94d4c9e0d5e227adb649641005553
Zinger <mnitpro@gmail.com>
2023-10-15 00:48:03 +0300

    fuxi: Fix Battery Stats/Health info

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -72,10 +72,10 @@ start_charger_monitor()
 start_vm_bms()
 {
 	if [ -e /dev/vm_bms ]; then
-		chown -h root.system /sys/class/power_supply/bms/current_now
-		chown -h root.system /sys/class/power_supply/bms/voltage_ocv
-		chmod 0664 /sys/class/power_supply/bms/current_now
-		chmod 0664 /sys/class/power_supply/bms/voltage_ocv
+		chown -h root.system /sys/class/power_supply/battery/current_now
+		chown -h root.system /sys/class/power_supply/battery/voltage_ocv
+		chmod 0664 /sys/class/power_supply/battery/current_now
+		chmod 0664 /sys/class/power_supply/battery/voltage_ocv
 		start vm_bms
 	fi
 }
