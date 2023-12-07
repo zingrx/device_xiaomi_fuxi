6cc8a0bf5f3ac8c8f190aa051da415e80f3fd4fc
Zinger <mnitpro@gmail.com>
2023-12-07 23:47:43 +0300

    sepolicy: Addressing vendor_sysfs_battery_supply error
    ** ERROR 'type vendor_sysfs_battery_supply is not defined or is an attribute'

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -201,6 +201,8 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_SEPOLICY_M4DEFS += \
+    sysfs_battery_supply=vendor_sysfs_battery_supply
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
